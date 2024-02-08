1539f04e20b33c70c66033fe3c208295e6d53159
Yuri Maciel <yurimpro@outlook.com>
2024-02-08 06:29:33 -0300

    add login page and update shadcn theme

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,10 +1,34 @@
+import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign In</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              LogIn panel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Track your sales through the partner panel
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">E-mail</Label>
+              <Input id="email" type="email"></Input>
+            </div>
+            <Button className="w-full" type="submit">
+              LogIn
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
